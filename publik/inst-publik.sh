860662bb71f748f4a9e2f18fca58f9aae378859f
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-11-29 23:58:18 +0700

    a

--- publik/inst-publik.sh
+++ publik/inst-publik.sh
@@ -3,15 +3,18 @@
 wget -O /usr/local/bin/add-ssh-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/add-ssh-user.sh" && chmod +x /usr/local/bin/add-ssh-user
 
 #add user shadowsocks
-wget -O /usr/local/bin/add-shadowsocks-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/hide/add-shadowsocks-user.sh" && chmod +x /usr/local/bin/add-shadowsocks-user
+#wget -O /usr/local/bin/add-shadowsocks-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/hide/add-shadowsocks-user.sh" && chmod +x /usr/local/bin/add-shadowsocks-user
 #add user trojan
-wget -O /usr/local/bin/add-trojango-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/hide/add-trojango-user.sh" && chmod +x /usr/local/bin/add-trojango-user
+wget -O /usr/local/bin/add-trojango-user "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/publik/add-trojango-user.sh" && chmod +x /usr/local/bin/add-trojango-user
 #add user v2ray
-wget -O /usr/local/bin/add-v2ray-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/hide/add-v2ray-user.sh" && chmod +x /usr/local/bin/add-v2ray-user
+wget -O /usr/local/bin/add-v2ray-user "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/publik/add-v2ray-user.sh" && chmod +x /usr/local/bin/add-v2ray-user
 
-#delete user tojan
-wget -O /usr/local/bin/del-trojango-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/hide/del-trojango-user.sh" && chmod +x /usr/local/bin/del-trojango-user
-#delete user v2ray
-wget -O /usr/local/bin/del-v2ray-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/hide/del-v2ray-user.sh" && chmod +x /usr/local/bin/del-v2ray-user
+#auto deleted akun 
+wget -O /usr/local/bin/del-vmess-user "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/packagepanel/del-vmess-user.sh" && chmod +x /usr/local/bin/del-vmess-user
+wget -O /usr/local/bin/del-trojan-user "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/packagepanel/del-trojan-user.sh" && chmod +x /usr/local/bin/del-trojan-user
 #delete user shadowsocks
-wget -O /usr/local/bin/trvmss-del-exp "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/hide/trvmss-del-exp.sh" && chmod +x /usr/local/bin/trvmss-del-exp
+#wget -O /usr/local/bin/trvmss-del-exp "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/hide/trvmss-del-exp.sh" && chmod +x /usr/local/bin/trvmss-del-exp
+
+#hapus dokument
+cd
+rm -rf inst-publik.sh
