fdbbf1fdad55d4e027c663ae9fc8fb5edd9e8ea7
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-12-05 11:53:09 +0700

    a

--- custompath/chg-path.sh
+++ custompath/chg-path.sh
@@ -5,12 +5,10 @@ echo $path >> /root/path.txt
 path1=$(cat /root/path.txt)
 path2="s/hhhhhhh/$path1/g";
 
-
-
 #Edit CONF VMESS
-wget -O /etc/xray/vmess-ws.json "https://raw.githubusercontent.com/4hidessh/hidessh/main/xray/vmess-ws.json"
+wget -O /etc/xray/vmess-ws.json "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/custompath/custom-vmess.sh"
 sed -i $path2 /etc/xray/vmess-ws.json
 
 
 #add user Vmess
-wget -O /etc/xray/add-vmess.sh "https://raw.githubusercontent.com/4hidessh/hidessh/main/xray/add-vmess.sh"
+wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/custompath/add-vmess5.sh"
