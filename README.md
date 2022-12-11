b7e3af0c0e1a2ef9cd63f1d077486ce1f3fe6709
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-12-11 23:25:36 +0700

    a

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ wget https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/install.s
 
 #custom 
 wget https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/custompath/chg-path.sh;chmod +x chg-path.sh;./chg-path.sh
+
+
+wget https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/inst-vpsv2.sh;chmod +x inst-vpsv2.sh;./inst-vpsv2.sh
+
+
+wget https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/xray/inst-tmbh.sh;chmod +x inst-tmbh.sh;./inst-tmbh.sh
--- xray/inst-tmbh.sh
+++ xray/inst-tmbh.sh
@@ -3,11 +3,12 @@
 #wget -q -O /usr/local/bin/add-ssh-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/add-ssh-user.sh" && chmod +x /usr/local/bin/add-ssh-user
 
 #worry free
-wget -q -O /etc/xray/svr-vmess-worry "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/xray/svr-vmess-worry.json"
-wget -q -O /etc/xray/svr-trojan-worry "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/xray/svr-trojan-worry.json"
+wget -q -O /etc/xray/svr-vmess-worry.json "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/xray/svr-vmess-worry.json"
+wget -q -O /etc/xray/svr-trojan-worry.json "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/xray/svr-trojan-worry.json"
+
 
 #kouta habis
-
+cd
 #worry free vmess ws
 cat> /etc/systemd/system/vmess-worry.service << END
 [Unit]
@@ -20,7 +21,7 @@ User=nobody
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/local/bin/xray run -config /etc/xray/svr-vmess-worry
+ExecStart=/usr/local/bin/xray run -config /etc/xray/svr-vmess-worry.json
 Restart=on-failure
 RestartPreventExitStatus=23
 LimitNPROC=10000
@@ -42,7 +43,7 @@ User=nobody
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/local/bin/xray run -config /etc/xray/svr-trojan-worry
+ExecStart=/usr/local/bin/xray run -config /etc/xray/svr-trojan-worry.json
 Restart=on-failure
 RestartPreventExitStatus=23
 LimitNPROC=10000
