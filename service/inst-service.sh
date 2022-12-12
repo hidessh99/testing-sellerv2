88f46ecfd142f95456bc77371a582a6155ab1157
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-12-12 21:54:13 +0700

    a

--- service/inst-service.sh
+++ service/inst-service.sh
@@ -1,7 +1,26 @@
 #bin bash
+cd
+wget -q -O /etc/systemd/system/trojan-tcp.service "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/service/trojan-tcp.service"
+wget -q -O /etc/systemd/system/ntls.service "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/service/ntls.service"
+#systemctl vless
+wget -q -O /etc/systemd/system/vless-ws.service "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/service/vless-ws.service"
+wget -q -O /etc/systemd/system/vless-grpc.service "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/service/vless-grpc.service"
+#systemctl vmess
+wget -q -O /etc/systemd/system/vmess-ws.service "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/service/vmess-ws.service"
+wget -q -O /etc/systemd/system/vmess-grpc.service "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/service/vmess-grpc.service"
+#systemctl trojan
+wget -q -O /etc/systemd/system/trojan-ws.service "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/service/trojan-ws.service"
+wget -q -O /etc/systemd/system/trojan-grpc.service "https://raw.githubusercontent.com/hidessh99/testing-sellerv2/main/service/trojan-grpc.service"
 
-wget -q -O /etc/systemd/system/trojan-tcp.service 
-wget -q -O /etc/systemd/system/ntls.service
+#permission service
+chmod +x /etc/systemd/system/trojan-tcp.service
+chmod +x /etc/systemd/system/ntls.service
+chmod +x /etc/systemd/system/vless-ws.service
+chmod +x /etc/systemd/system/vless-grpc.service
+chmod +x /etc/systemd/system/vmess-ws.service
+chmod +x /etc/systemd/system/vmess-grpc.service
+chmod +x /etc/systemd/system/trojan-ws.service
+chmod +x /etc/systemd/system/trojan-grpc.service
 
-
-
+#deleted file
+rm -rf /root/inst-service.sh
